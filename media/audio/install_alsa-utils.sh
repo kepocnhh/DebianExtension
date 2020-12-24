67d81895dd751138b107aacd31961258fc1ba1ec
Stanley Wintergreen <kepocnhh@gmail.com>
2020-12-24 21:05:50 +0300

    wip | script install alsa-utils

---
+++ media/audio/install_alsa-utils.sh
@@ -0,0 +1,10 @@
+ERROR_CODE_SERVICE=1
+
+if test -z $DEBIAN_EXTENSION_HOME; then
+    echo "Debian extension home path must be not empty!"
+    exit $ERROR_CODE_SERVICE
+fi
+
+$DEBIAN_EXTENSION_HOME/common/install_package.sh alsa-utils
+
+exit 0
