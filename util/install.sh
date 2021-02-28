c7c5c63ac4a6d5489d4fc5f72177cb4330286b85
Stanley Wintergreen <kepocnhh@gmail.com>
2021-02-28 03:14:17 +0300

    wip | install util

---
+++ util/install.sh
@@ -0,0 +1,26 @@
+#!/bin/bash
+
+echo "install util..."
+
+ERROR_CODE_SERVICE=1
+ERROR_CODE_INSTALL=100
+
+if test -z $DEBIAN_EXTENSION_HOME; then
+    echo "Debian extension home path must be not empty!"
+    exit $ERROR_CODE_SERVICE
+fi
+
+array=("curl" "default" "exfat-fuse" "openssl" "unzip" "usb_mount")
+SIZE=${#array[@]}
+for ((i = 0; i < SIZE; i++)); do
+ ITEM="${array[$i]}"
+ $DEBIAN_EXTENSION_HOME/util/install_${ITEM}.sh
+ if test $? -ne 0; then
+  echo "Install \"$ITEM\" error!"
+  exit $((ERROR_CODE_INSTALL + i))
+ fi
+done
+
+echo "install util success."
+
+exit 0
