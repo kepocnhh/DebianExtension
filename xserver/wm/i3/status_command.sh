fb0908b4996f5c19af3b2f0e4b2ed97253bd477a
Stanley Wintergreen <kepocnhh@gmail.com>
2021-04-19 00:32:24 +0300

    wip | i3 status fix...

--- xserver/wm/i3/status_command.sh
+++ xserver/wm/i3/status_command.sh
@@ -7,6 +7,7 @@ if test -z $DEBIAN_EXTENSION_HOME; then
     exit $ERROR_CODE_SERVICE
 fi
 
+COLOR_GREEN='#66BB6A'
 echo '{"version":1}'
 echo '['
 echo '[]'
@@ -14,6 +15,9 @@ echo '[]'
 while :; do
  TIME=$(date +"%Y/%m/%d %H:%M:%S")
  RESULT="{\"full_text\":\"$TIME\"}"
+
+ SSID=$($DEBIAN_EXTENSION_HOME/network/wireless/get_wireless_ssid.sh $WF_NI_MAIN)
+ [ $? -eq 0 ] && RESULT="{\"full_text\":\"$WF_NI_MAIN/$SSID\",\"color\":\"$COLOR_GREEN\"},$RESULT"
 
  MOUNT=$($DEBIAN_EXTENSION_HOME/util/mount/get_mount_by_device.sh)
  [ $? -eq 0 ] && RESULT="{\"full_text\":\"$MOUNT\"},$RESULT"
