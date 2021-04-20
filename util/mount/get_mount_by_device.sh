8319eabf527caf76a2c49e6fcd53eb5000f18605
Stanley Wintergreen <kepocnhh@gmail.com>
2021-04-20 20:42:38 +0300

    wip | util mount script fix...

--- util/mount/get_mount_by_device.sh
+++ util/mount/get_mount_by_device.sh
@@ -3,37 +3,31 @@
 ERROR_CODE_DEVICE_EXIST=11
 ERROR_CODE_DEVICE_EMPTY=21
 ERROR_CODE_RESULT_EMPTY=31
-ERROR_CODE_MOUNTS_EMPTY=41
 
-tmp=$(ls /dev/sd*)
+tmp=$(ls /dev/sd* | grep -E "sd[a-z][0-9]")
 if test $? -ne 0; then
  echo "Device must exist!"
  exit $ERROR_CODE_DEVICE_EXIST
 fi
-
-tmp=$(grep -E "sd[a-z][0-9]" <<< $tmp)
 if test -z "$tmp"; then
  echo "Device empty!"
  exit $ERROR_CODE_DEVICE_EMPTY
 fi
 
-array=(${tmp//$'\n'/ })
-SIZE=${#array[@]}
-
-MOUNTS=$(cat /proc/mounts)
-if test -z "$MOUNTS"; then
- echo "Mounts empty!"
- exit $ERROR_CODE_MOUNTS_EMPTY
-fi
+ARRAY=(${tmp//$'\n'/ })
+SIZE=${#ARRAY[@]}
 
 RESULT=""
 for ((i = 0; i < SIZE; i++)); do
- DEVICE="${array[$i]}"
+ DEVICE="${ARRAY[$i]}"
+ MOUNTPOINT=$(/usr/bin/lsblk -nb "$DEVICE" -o MOUNTPOINT)
  [[ $? -ne 0 ]] && continue
- INFO=$(grep "$DEVICE" <<< "$MOUNTS")
- if test -n "$INFO"; then
-  tmp=($INFO)
-  RESULT="${tmp[1]} $RESULT"
+ if test -n "$MOUNTPOINT"; then
+  if test -z "$RESULT"; then
+   RESULT="$MOUNTPOINT"
+  else
+   RESULT="$MOUNTPOINT $RESULT"
+  fi
  fi
 done
 
