107c39ff4028733f1d14b4f1b368e6003943721b
Stanley Wintergreen <kepocnhh@gmail.com>
2021-04-28 17:28:33 +0300

    wip | usb mount fstype support

--- util/mount/usb_mount.sh
+++ util/mount/usb_mount.sh
@@ -7,6 +7,8 @@ ERROR_CODE_DEVICE_EMPTY=21
 ERROR_CODE_DEVICE_EXISTS=22
 ERROR_CODE_MOUNTPOINT_EXISTS=31
 ERROR_CODE_MOUNTPOINT_CHANGE_OWNER=32
+ERROR_CODE_FSTYPE_GET=41
+ERROR_CODE_FSTYPE_UNSUPPORTED=42
 ERROR_CODE_MOUNT=5
 
 CODE=0
@@ -30,6 +32,12 @@ else
  exit $ERROR_CODE_DEVICE_EXISTS
 fi
 
+FSTYPE=$(/usr/bin/lsblk -nb "$DEVICE" -o FSTYPE); CODE=$?
+if test $CODE -ne 0; then
+ echo "Get fstype \"$DEVICE\" error $CODE!"
+ exit $ERROR_CODE_FSTYPE_GET
+fi
+
 MOUNTPOINT="${DEVICE}.m"
 if test -d "$MOUNTPOINT"; then
  echo "Mount point \"$DEVICE\" exists!"
@@ -43,7 +51,16 @@ else
  fi
 fi
 
-/usr/bin/mount -o umask=0 $DEVICE $MOUNTPOINT; CODE=$?
+case "$FSTYPE" in
+ "iso9660") /usr/bin/mount -t $FSTYPE $DEVICE $MOUNTPOINT;;
+ "vfat") /usr/bin/mount -t $FSTYPE -o umask=0 $DEVICE $MOUNTPOINT;;
+ "exfat") /usr/bin/mount -t $FSTYPE $DEVICE $MOUNTPOINT;;
+ *)
+  echo "Fstype \"$FSTYPE\" of \"$DEVICE\" unsupported!"
+  exit $ERROR_CODE_FSTYPE_UNSUPPORTED
+ ;;
+esac
+CODE=$?
 if test $CODE -ne 0; then
  echo "Mount \"$DEVICE\" error $CODE!"
  exit $ERROR_CODE_MOUNT
