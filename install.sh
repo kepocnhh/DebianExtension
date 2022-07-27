7132c23c62a120f541115b4a28b50e3ee8237c4d
Stanley Wintergreen <kepocnhh@gmail.com>
2022-07-27 16:32:58 +0300

    install repository

--- install.sh
+++ install.sh
@@ -5,25 +5,33 @@ REPOSITORY_NAME="DebianExtension"
 
 echo "Install ${REPOSITORY_OWNER}/${REPOSITORY_NAME}..."
 
+if test $# -ne 1; then
+  echo "Script needs for 1 arguments but actual $#!"; exit 21
+fi
+
+VERSION=$1
+
+for it in VERSION; do
+ if test -z "${!it}"; then echo "$it is empty!"; exit 22; fi; done
+
 /usr/bin/apt-get install --no-install-recommends -y git ca-certificates
 if test $? -ne 0; then
  echo "Install required error!"; exit 11
 fi
 
-PATH="/usr/local/bin/DebianExtension"
-if test -d "$PATH"; then
- /usr/bin/rm -rf $PATH
- if test $? -ne 0; then
-  echo "Delete \"$PATH\" error!"; exit 12
- fi
-fi
+FILE_PATH="/usr/local/bin/DebianExtension"
+rm -rf $FILE_PATH
+mkdir -p $FILE_PATH
 
-/usr/bin/git clone https://github.com/${REPOSITORY_OWNER}/${REPOSITORY_NAME}.git $PATH
+git -C $FILE_PATH init \
+ && git -C $FILE_PATH remote add origin https://github.com/${REPOSITORY_OWNER}/${REPOSITORY_NAME}.git \
+ && git -C $FILE_PATH fetch --depth=1 origin $VERSION \
+ && git -C $FILE_PATH checkout FETCH_HEAD
 if test $? -ne 0; then
  echo "Clone error!"; exit 13
 fi
 
-echo "DEBIAN_EXTENSION_HOME=$PATH" >> /etc/environment
+echo "DEBIAN_EXTENSION_HOME=$FILE_PATH" >> /etc/environment
 
 echo "Install Debian Extension success."
 
