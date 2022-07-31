b30276a932965cb8781027150d6c12394e8b6c47
Stanley Wintergreen <kepocnhh@gmail.com>
2022-07-31 22:22:49 +0300

    fix install xserver

--- xserver/install.sh
+++ xserver/install.sh
@@ -6,10 +6,10 @@ if [ ! -d "$DEBIAN_EXTENSION_HOME" ]; then
  echo "Dir $DEBIAN_EXTENSION_HOME does not exist!"; exit 11
 fi
 
-$DEBIAN_EXTENSION_HOME/common/install_package.sh libpam-systemd || exit 21
-$DEBIAN_EXTENSION_HOME/common/install_package.sh xinit || exit 22
-$DEBIAN_EXTENSION_HOME/common/install_package.sh x11-xsxerver-utils || exit 23
-$DEBIAN_EXTENSION_HOME/common/install_package.sh dbus-x11 || exit 24
+ARRAY=("libpam-systemd" xinit "x11-xsxerver-utils" "x11-utils" "dbus-x11")
+for ((i = 0; i < ${#ARRAY[@]}; i++)); do
+ $DEBIAN_EXTENSION_HOME/common/install_package.sh "${ARRAY[$i]}" || exit $((20 + i))
+done
 
 touch $HOME/.bash_aliases
 echo "alias sx=\"/usr/bin/startx && exit\"" >> $HOME/.bash_aliases
