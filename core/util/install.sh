e4af66948c5e3b3d4a3d8cd423e8c2b4f94f92c1
Stanley Wintergreen <kepocnhh@gmail.com>
2022-07-31 19:05:19 +0300

    fix install util

--- core/util/install.sh
+++ core/util/install.sh
@@ -6,7 +6,14 @@ if [ ! -d "$DEBIAN_EXTENSION_HOME" ]; then
  echo "Dir $DEBIAN_EXTENSION_HOME does not exist!"; exit 11
 fi
 
-ARRAY=(curl default "exfat-fuse" openssl unzip "usb_mount_service" git)
+
+$DEBIAN_EXTENSION_HOME/common/install_package.sh curl && curl --version || exit 31
+$DEBIAN_EXTENSION_HOME/common/install_package.sh openssl && openssl version -a || exit 31
+$DEBIAN_EXTENSION_HOME/common/install_package.sh exfat-fuse || exit 32
+$DEBIAN_EXTENSION_HOME/common/install_package.sh unzip || exit 32
+$DEBIAN_EXTENSION_HOME/common/install_package.sh lbzip2 || exit 32
+
+ARRAY=(default "usb_mount_service" git)
 for ((i = 0; i < ${#ARRAY[@]}; i++)); do
  ITEM="${ARRAY[$i]}"
  $DEBIAN_EXTENSION_HOME/core/util/install_${ITEM}.sh
