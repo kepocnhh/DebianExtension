28eca62e7ea2e299d2e031c5f3c24ed281b17122
Stanley Wintergreen <kepocnhh@gmail.com>
2022-07-31 19:38:24 +0300

    install firefox lib

--- xserver/desktop/install_firefox.sh
+++ xserver/desktop/install_firefox.sh
@@ -12,7 +12,12 @@ for it in FIREFOX_VERSION DISTRIBUTION LANGUAGE; do
  if test -z "${!it}"; then echo "$it is empty!"; exit 13; fi; done
 
 if test -d "/opt/mozilla/firefox-${FIREFOX_VERSION}"; then
- echo "Firefox ${FIREFOX_VERSION} exists!"; exit 101
+ echo "Firefox ${FIREFOX_VERSION} exists!"; exit 14
+fi
+
+sudo apt-get install --no-install-recommends -y libgtk-3-0 libdbus-glib-1-2
+if test $? -ne 0; then
+ echo "Insall lib error!"; exit 21
 fi
 
 URL="https://ftp.mozilla.org/pub/firefox/releases/$FIREFOX_VERSION/$DISTRIBUTION/$LANGUAGE"
@@ -22,14 +27,14 @@ FILE="firefox-${FIREFOX_VERSION}.tar.bz2"
 rm /tmp/${FILE}
 curl -f "$URL/$FILE" -o /tmp/$FILE
 if test $? -ne 0; then
- echo "Download firefox error!"; exit 11
+ echo "Download firefox error!"; exit 31
 fi
 
 echo "Unzip firefox ${FIREFOX_VERSION}..."
 rm -rf /tmp/firefox
 tar -xf /tmp/${FILE} -C /tmp
 if test $? -ne 0; then
- echo "Unzip firefox error!"; exit 21
+ echo "Unzip firefox error!"; exit 32
 fi
 
 echo "Install firefox ${FIREFOX_VERSION}..."
@@ -38,14 +43,14 @@ if [ ! -d "/opt/mozilla" ]; then
 fi
 sudo mv /tmp/firefox "/opt/mozilla/firefox-${FIREFOX_VERSION}"
 if test $? -ne 0; then
- echo "Install firefox error!"; exit 31
+ echo "Install firefox error!"; exit 33
 fi
 
 rm /tmp/${FILE}
 echo "Running firefox ${FIREFOX_VERSION}..."
 /opt/mozilla/firefox-${FIREFOX_VERSION}/firefox --version
 if test $? -ne 0; then
- echo "Running firefox error!"; exit 41
+ echo "Running firefox error!"; exit 34
 fi
 
 exit 0
