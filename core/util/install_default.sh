cfa9e8dcfd8fadad38d19b60f724b0e53ee62caf
Stanley Wintergreen <kepocnhh@gmail.com>
2022-07-31 18:39:24 +0300

    fix install default

--- core/util/install_default.sh
+++ core/util/install_default.sh
@@ -9,7 +9,7 @@ fi
 DEFAULT_PATH="/etc/default"
 ARRAY=("console-setup" "keyboard")
 for ((i = 0; i < ${#ARRAY[@]}; i++)); do
- FILE_NAME="${array[$i]}"
+ FILE_NAME="${ARRAY[$i]}"
  RESULT_PATH="$DEFAULT_PATH/$FILE_NAME"
  rm "$RESULT_PATH"
  cp "$DEBIAN_EXTENSION_HOME/debian$RESULT_PATH" "$RESULT_PATH"
