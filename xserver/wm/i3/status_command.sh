6b316229bf915c36771cb39f232f541212bfdda6
Stanley Wintergreen <kepocnhh@gmail.com>
2022-07-31 19:21:23 +0300

    fix status command

--- xserver/wm/i3/status_command.sh
+++ xserver/wm/i3/status_command.sh
@@ -13,10 +13,10 @@ while :; do
  TIME=$(date +"%Y/%m/%d %H:%M:%S")
  RESULT="{\"full_text\":\"$TIME\"}"
 
-# SSID=$($DEBIAN_EXTENSION_HOME/network/wireless/get_wireless_ssid.sh $WF_NI_MAIN)
+# SSID=$($DEBIAN_EXTENSION_HOME/core/network/wireless/get_wireless_ssid.sh $WF_NI_MAIN)
 # [ $? -eq 0 ] && RESULT="{\"full_text\":\"$WF_NI_MAIN/$SSID\",\"color\":\"$COLOR_GREEN\"},$RESULT"
 
- MOUNT=$($DEBIAN_EXTENSION_HOME/util/mount/get_mount_by_device.sh)
+ MOUNT=$($DEBIAN_EXTENSION_HOME/core/util/mount/get_mount_by_device.sh)
  [ $? -eq 0 ] && RESULT="{\"full_text\":\"$MOUNT\"},$RESULT"
 
  echo ",[$RESULT]"
