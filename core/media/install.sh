3eec5598baad3186b278c73bdd84cbad45f0be85
Stanley Wintergreen <kepocnhh@gmail.com>
2022-07-31 21:36:08 +0300

    fix media

--- core/media/install.sh
+++ core/media/install.sh
@@ -6,18 +6,17 @@ if [ ! -d "$DEBIAN_EXTENSION_HOME" ]; then
  echo "Dir $DEBIAN_EXTENSION_HOME does not exist!"; exit 11
 fi
 
-ARRAY=(\
-"audio/install_alsa-utils.sh" \
-"audio/install_pulseaudio.sh" \
-"install_playerctl.sh")
+ARRAY=("alsa-utils" pulseaudio playerctl)
 for ((i = 0; i < ${#ARRAY[@]}; i++)); do
  ITEM="${ARRAY[$i]}"
- $DEBIAN_EXTENSION_HOME/core/media/$ITEM
+ $DEBIAN_EXTENSION_HOME/common/install_package.sh $ITEM
  if test $? -ne 0; then
   echo "Install \"$ITEM\" error!"; exit $((20 + i))
  fi
 done
 
-echo "Install media success."
+aplay --version
+pulseaudio --version
+playerctl --version
 
 exit 0
