a360ac4aeb26591ce2497ce0732f9aba1162ffda
Stanley Wintergreen <kepocnhh@gmail.com>
2022-08-02 17:00:56 +0300

    fix wireless

--- core/network/wireless/wireless_up.sh
+++ core/network/wireless/wireless_up.sh
@@ -1,74 +1,48 @@
 #!/bin/bash
 
-echo "wireless up..."
-
-ERROR_CODE_EXTENSION_HOME=10
-ERROR_CODE_SERVICE=11
-ERROR_CODE_EMPTY_NETWORK_INTERFACE_NAME=2
-ERROR_CODE_EMPTY_SSID=3
-ERROR_CODE_NETWORK_INTERFACE_STATE=4
-ERROR_CODE_NETWORK_INTERFACE_UP=50
-ERROR_CODE_ACCESS_POINT_CONNECT=60
-ERROR_CODE_DHCLIENT=70
-
-ARGUMENT_COUNT_EXPECTED=2
-if test $# -ne $ARGUMENT_COUNT_EXPECTED; then
-    echo "Script needs for $ARGUMENT_COUNT_EXPECTED arguments but actual $#"
-    exit $ERROR_CODE_SERVICE
+if test $# -ne 2; then
+  echo "Script needs for 2 arguments but actual $#!"; exit 11
 fi
 
 NI_NAME=$1
 SSID=$2
 
-if test -z $NI_NAME; then
-    echo "Network interface name must be not empty!"
-    exit $ERROR_CODE_EMPTY_NETWORK_INTERFACE_NAME
-fi
-if test -z $SSID; then
-    echo "SSID must be not empty!"
-    exit $ERROR_CODE_EMPTY_SSID
-fi
+for it in NI_NAME SSID; do
+ if test -z "${!it}"; then echo "$it is empty!"; exit 12; fi; done
 
 NI_STATE_UP=0x1003
 NI_STATE_DOWN=0x1002
 NI_STATE=$(cat /sys/class/net/$NI_NAME/flags)
 
-CODE=0
 case "$NI_STATE" in
  "$NI_STATE_UP")
   echo "Network interface state already up..."
  ;;
  "$NI_STATE_DOWN")
-  /usr/bin/ip link set $NI_NAME up; CODE=$?
-  if test $CODE -ne 0; then
-   echo "Network interface up error $CODE!"
-   exit $ERROR_CODE_NETWORK_INTERFACE_UP
+  /usr/bin/ip link set $NI_NAME up
+  if test $? -ne 0; then
+   echo "Network interface up error!"; exit 21
   fi
  ;;
  *)
-  echo "Network interface state expected \"$NI_STATE_UP\" or \"$NI_STATE_DOWN\" but actual \"$NI_STATE\"!"
-  exit $ERROR_CODE_NETWORK_INTERFACE_STATE
+  echo "Network interface state expected \"$NI_STATE_UP\" or \"$NI_STATE_DOWN\" but actual \"$NI_STATE\"!"; exit 22
  ;;
 esac
 
-#DRIVER=wext
-DRIVER=nl80211
+# DRIVER=wext # legacy Linux wireless extensions ioctl-based interface
+DRIVER=nl80211 # modern Linux nl80211/cfg80211 netlink-based interface
 /usr/sbin/wpa_supplicant \
-    -B \
-    -D $DRIVER \
-    -i $NI_NAME \
-    -c "/etc/wpa_supplicant.$SSID.conf"
+ -B \
+ -D $DRIVER \
+ -i $NI_NAME \
+ -c "/etc/wpa_supplicant.${SSID}.conf"
 if test $? -ne 0; then
-  echo "Connect to access point \"$SSID\" error!"
- exit $ERROR_CODE_ACCESS_POINT_CONNECT
+ echo "Connect to access point \"$SSID\" error!"; exit 31
 fi
 
-/usr/sbin/dhclient $NI_NAME; CODE=$?
-if test $CODE -ne 0; then
- echo "dhclient error $CODE!"
- exit $ERROR_CODE_DHCLIENT
+/usr/sbin/dhclient $NI_NAME
+if test $? -ne 0; then
+ echo "dhclient error!"; exit 32
 fi
-
-echo "wireless $NI_NAME/$SSID up success."
 
 exit 0
--- xserver/install.sh
+++ xserver/install.sh
@@ -2,17 +2,15 @@
 
 echo "Install xserver..."
 
-if [ ! -d "$DEBIAN_EXTENSION_HOME" ]; then
- echo "Dir $DEBIAN_EXTENSION_HOME does not exist!"; exit 11
-fi
+for it in HOME DEBIAN_EXTENSION_HOME; do
+ if test -d "${!it}"; then echo "Dir $it does not exist!"; exit 11; fi; done
 
 ARRAY=("libpam-systemd" xinit "x11-xserver-utils" "x11-utils" "dbus-x11")
 for ((i = 0; i < ${#ARRAY[@]}; i++)); do
  $DEBIAN_EXTENSION_HOME/common/install_package.sh "${ARRAY[$i]}" || exit $((20 + i))
 done
 
-touch $HOME/.bash_aliases
-echo "alias sx=\"/usr/bin/startx && exit\"" >> $HOME/.bash_aliases
+echo "alias sx=\"/usr/bin/startx; exit\"" >> $HOME/.bash_aliases
 
 $DEBIAN_EXTENSION_HOME/xserver/xorg/install.sh || exit 31
 
