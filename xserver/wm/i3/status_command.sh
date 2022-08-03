08f3ab11a7c3a4c27083ce96d07233dc9713a930
Stanley Wintergreen <kepocnhh@gmail.com>
2022-08-03 03:02:15 +0300

    status command bt

--- xserver/wm/i3/status_command.sh
+++ xserver/wm/i3/status_command.sh
@@ -4,14 +4,23 @@ if [ ! -d "$DEBIAN_EXTENSION_HOME" ]; then
  echo "Dir $DEBIAN_EXTENSION_HOME does not exist!"; exit 11
 fi
 
-COLOR_GREEN='#66BB6A'
-echo '{"version":1}'
+COLOR_GRAY='#888888'
+COLOR_GREEN='#43a047'
+COLOR_BLUE='#1976d2'
+COLOR_WHITE='#ffffff'
+echo '{"version":1},"click_events":true'
 echo '['
 echo '[]'
 
-while :; do
+(while :; do
  TIME=$(date +"%Y/%m/%d %H:%M:%S")
  RESULT="{\"full_text\":\"$TIME\"}"
+
+ STATUS=$($DEBIAN_EXTENSION_HOME/core/network/bluetooth/is_powered.sh "$BT_MAC_CONTROLLER") \
+  && case "$STATUS" in
+  yes);;
+  no) RESULT="{\"name\":\"bt_off\",\"full_text\":\"B\",\"color\":\"$COLOR_GRAY\"},$RESULT";;
+ esac
 
 # SSID=$($DEBIAN_EXTENSION_HOME/core/network/wireless/get_wireless_ssid.sh $WF_NI_MAIN)
 # [ $? -eq 0 ] && RESULT="{\"full_text\":\"$WF_NI_MAIN/$SSID\",\"color\":\"$COLOR_GREEN\"},$RESULT"
@@ -20,6 +29,14 @@ while :; do
  [ $? -eq 0 ] && RESULT="{\"full_text\":\"$MOUNT\"},$RESULT"
 
  echo ",[$RESULT]"
+done) &
+
+while read it; do
+ case "$(echo $it | jq -r .name)" in
+  bt_off) if test "$($DEBIAN_EXTENSION_HOME/core/network/bluetooth/is_powered.sh "$BT_MAC_CONTROLLER")" == no; then
+   /usr/bin/bluetoothctl power on > /dev/null &
+  fi;;
+ esac
 done
 
 exit 0
