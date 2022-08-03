8c530975de42d0e2b66db687ab2ab0f107874f4a
Stanley Wintergreen <kepocnhh@gmail.com>
2022-08-03 16:36:09 +0300

    fix status command

--- xserver/wm/i3/status_command.sh
+++ xserver/wm/i3/status_command.sh
@@ -5,6 +5,7 @@ if [ ! -d "$DEBIAN_EXTENSION_HOME" ]; then
 fi
 
 COLOR_GRAY='#888888'
+COLOR_RED='#d32f2f'
 COLOR_GREEN='#43a047'
 COLOR_BLUE='#1976d2'
 COLOR_WHITE='#ffffff'
@@ -16,15 +17,22 @@ echo '[]'
  TIME=$(date +"%Y/%m/%d %H:%M:%S")
  RESULT="{\"full_text\":\"$TIME\"}"
 
- STATUS=$($DEBIAN_EXTENSION_HOME/core/network/bluetooth/is_powered.sh "$BT_MAC_CONTROLLER") \
-  && case "$STATUS" in
-  yes) STATUS=$($DEBIAN_EXTENSION_HOME/core/network/bluetooth/is_connected.sh "$BT_MAC_SPEAKERS") \
-   && case "$STATUS" in
-   yes);;
-   no) RESULT="{\"name\":\"speakers_off\",\"full_text\":\"S\",\"color\":\"$COLOR_GRAY\"},$RESULT";;
-  esac;;
-  no) RESULT="{\"name\":\"bt_off\",\"full_text\":\"B\",\"color\":\"$COLOR_GRAY\"},$RESULT";;
- esac
+ CODE=0
+ STATUS=$($DEBIAN_EXTENSION_HOME/core/network/bluetooth/is_powered.sh "$BT_MAC_CONTROLLER")
+ if [ $CODE -eq 0 ] && [ "$STATUS" == yes ]; then
+  STATUS=$($DEBIAN_EXTENSION_HOME/core/network/bluetooth/is_connected.sh "$BT_MAC_SPEAKERS")
+  if [ $CODE -eq 0 ] && [ "$STATUS" == yes ]; then
+   RESULT="{\"name\":\"speakers_on\",\"full_text\":\"S\",\"color\":\"$COLOR_WHITE\"},$RESULT"
+  elif [ $CODE -eq 0 ] && [ "$STATUS" == no ]; then
+   RESULT="{\"name\":\"speakers_off\",\"full_text\":\"S\",\"color\":\"$COLOR_GRAY\"},$RESULT"
+  else
+   RESULT="{\"full_text\":\"S\",\"color\":\"$COLOR_RED\"},$RESULT"
+  fi
+ elif [ $CODE -eq 0 ] && [ "$STATUS" == no ]; then
+  RESULT="{\"name\":\"bt_off\",\"full_text\":\"B\",\"color\":\"$COLOR_GRAY\"},$RESULT"
+ else
+  RESULT="{\"full_text\":\"B\",\"color\":\"$COLOR_RED\"},$RESULT"
+ fi
 
 # SSID=$($DEBIAN_EXTENSION_HOME/core/network/wireless/get_wireless_ssid.sh $WF_NI_MAIN)
 # [ $? -eq 0 ] && RESULT="{\"full_text\":\"$WF_NI_MAIN/$SSID\",\"color\":\"$COLOR_GREEN\"},$RESULT"
@@ -42,6 +50,9 @@ while read it; do
   fi;;
   speakers_off) if test "$($DEBIAN_EXTENSION_HOME/core/network/bluetooth/is_connected.sh "$BT_MAC_SPEAKERS")" == no; then
    /usr/bin/bluetoothctl connect $BT_MAC_SPEAKERS > /tmp/bt_connect.log &
+  fi;;
+  speakers_on) if test "$($DEBIAN_EXTENSION_HOME/core/network/bluetooth/is_connected.sh "$BT_MAC_SPEAKERS")" == yes; then
+   /usr/bin/bluetoothctl disconnect $BT_MAC_SPEAKERS > /tmp/bt_disconnect.log &
   fi;;
  esac
 done
