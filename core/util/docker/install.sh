eec30c2831949b865ecfca6bfe3c83d1718e9ad6
Stanley Wintergreen <kepocnhh@gmail.com>
2022-08-17 17:24:47 +0300

    docker | fix manually

--- core/util/docker/install.sh
+++ core/util/docker/install.sh
@@ -9,11 +9,11 @@ MACHINE_HARDWARE_NAME="$(/usr/bin/uname -m)"
 . /etc/os-release
 
 for it in MACHINE_HARDWARE_NAME ID VERSION_CODENAME; do
- if test -z "${!it}"; then echo "$it is empty!"; exit 11; fi; done
+ if test -z "${!it}"; then echo "$it is empty!"; exit 12; fi; done
 
 case "$MACHINE_HARDWARE_NAME" in
  'x86_64') ARCHITECTURE=amd64;;
- *) echo "Architecture $MACHINE_HARDWARE_NAME is not supported!"; exit 12;;
+ *) echo "Architecture $MACHINE_HARDWARE_NAME is not supported!"; exit 13;;
 esac
 
 VERSIONS="$($DEBIAN_EXTENSION_HOME/core/util/docker/versions.sh)"
@@ -67,9 +67,14 @@ while : ; do
 done
 
 for it in DOCKER_VERSION CONTAINERD_VERSION DOCKERD_VERSION; do
- if test -z "${!it}"; then echo "$it is empty!"; exit 11; fi; done
+ if test -z "${!it}"; then echo "$it is empty!"; exit 14; fi; done
 
-exit 1 # todo
+$DEBIAN_EXTENSION_HOME/core/util/docker/install_docker.sh $DOCKER_VERSION \
+ && $DEBIAN_EXTENSION_HOME/core/util/docker/install_containerd.sh $CONTAINERD_VERSION \
+ && $DEBIAN_EXTENSION_HOME/core/util/docker/install_dockerd.sh $DOCKERD_VERSION
+if test $? -ne 0; then
+ echo "Manually install docker error!"; exit 21
+fi
 
 echo "
 $ sudo groupadd docker
