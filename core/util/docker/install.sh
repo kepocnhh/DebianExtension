e0b5b525555be533b1c5c1673aee2293cf6a3fff
Stanley Wintergreen <kepocnhh@gmail.com>
2022-08-17 17:21:54 +0300

    docker | fix versions

--- core/util/docker/install.sh
+++ core/util/docker/install.sh
@@ -18,21 +18,56 @@ esac
 
 VERSIONS="$($DEBIAN_EXTENSION_HOME/core/util/docker/versions.sh)"
 
-LEFT='(?<=docker-ce-cli_">)'
+LEFT='(?<=docker-ce-cli_)'
 RIGHT="(?=~3-0~${ID}-${VERSION_CODENAME}_${ARCHITECTURE})"
 DOCKER_VERSION=""
 echo "
-$(echo $VERSIONS | grep -Po "${LEFT}\S+${RIGHT}")
+$(echo $VERSIONS | grep -Po "${LEFT}\S+${RIGHT}" | sort -V)
 
 Enter docker version:"
 while : ; do
-  read -n1 char
-  if test -z $char; then
-     echo; break
-  else
-     SSID+=$char
-  fi
+ read -n1 char
+ if test -z $char; then
+  echo; break
+ else
+  DOCKER_VERSION+=$char
+ fi
 done
+
+LEFT='(?<=containerd.io_)'
+RIGHT="(?=-1_${ARCHITECTURE})"
+CONTAINERD_VERSION=""
+echo "
+$(echo $VERSIONS | grep -Po "${LEFT}\S+${RIGHT}" | sort -V)
+
+Enter containerd version:"
+while : ; do
+ read -n1 char
+ if test -z $char; then
+  echo; break
+ else
+  CONTAINERD_VERSION+=$char
+ fi
+done
+
+LEFT='(?<=docker-ce_)'
+RIGHT="(?=~3-0~${ID}-${VERSION_CODENAME}_${ARCHITECTURE})"
+DOCKERD_VERSION=""
+echo "
+$(echo $VERSIONS | grep -Po "${LEFT}\S+${RIGHT}" | sort -V)
+
+Enter dockerd version:"
+while : ; do
+ read -n1 char
+ if test -z $char; then
+  echo; break
+ else
+  DOCKERD_VERSION+=$char
+ fi
+done
+
+for it in DOCKER_VERSION CONTAINERD_VERSION DOCKERD_VERSION; do
+ if test -z "${!it}"; then echo "$it is empty!"; exit 11; fi; done
 
 exit 1 # todo
 
