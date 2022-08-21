d691e42aaa258eb720b0818d04a83bbfd0f2760a
Stanley Wintergreen <kepocnhh@gmail.com>
2022-08-21 13:55:26 +0300

    fix install firefox

--- xserver/desktop/install_firefox.sh
+++ xserver/desktop/install_firefox.sh
@@ -1,83 +1,101 @@
 #!/bin/bash
 
-if test $# -ne 1; then
-  echo "Script needs for 1 arguments but actual $#!"; exit 11
+ISSUER=firefox
+
+LATEST_VERSIONS="$(curl -s https://ftp.mozilla.org/pub/firefox/releases/ \
+ | grep '/releases/' \
+ | grep -Po '(?<=/releases/)[1-9]([0-9]|\.)+[0-9](?=/">)' \
+ | sort -V | tail -n 16)"
+if test $? -ne 0; then
+ echo "Get latest versions $ISSUER error!"; exit 11
+elif test -z "$LATEST_VERSIONS"; then
+ echo "Latest versions $ISSUER is empty!"; exit 12
 fi
 
-FIREFOX_VERSION=$1
-DISTRIBUTION='linux-x86_64'
-LANGUAGE='en-US'
+ISSUER_VERSION=""
+echo "
+Latest 16 versions:
+$LATEST_VERSIONS
 
-for it in FIREFOX_VERSION DISTRIBUTION LANGUAGE; do
+Enter $ISSUER version:"
+while : ; do
+ read -n1 char
+ if test -z $char; then
+  echo; break
+ else
+  ISSUER_VERSION+=$char
+ fi
+done
+
+. /etc/os-release
+
+for it in MACHINE_HARDWARE_NAME; do
  if test -z "${!it}"; then echo "$it is empty!"; exit 21; fi; done
 
-if test -d "/opt/mozilla/firefox-$FIREFOX_VERSION"; then
- echo "Firefox $FIREFOX_VERSION exists!"; exit 22
+DISTRIBUTION="linux-$MACHINE_HARDWARE_NAME"
+LANGUAGE='en-US'
+
+for it in ISSUER_VERSION DISTRIBUTION LANGUAGE; do
+ if test -z "${!it}"; then echo "$it is empty!"; exit 22; fi; done
+
+if test -d "/opt/mozilla/firefox-$ISSUER_VERSION"; then
+ echo "${ISSUER^} $ISSUER_VERSION exists!"; exit 23
 fi
 
 case "$DISTRIBUTION" in
  'linux-x86_64') /bin/true;; # ignored
- *) echo "Distribution $DISTRIBUTION is not supported!"; exit 23;;
+ *) echo "Distribution $DISTRIBUTION is not supported!"; exit 31;;
 esac
 
 case "$LANGUAGE" in
  'en-US') /bin/true;; # ignored
- *) echo "Distribution $DISTRIBUTION is not supported!"; exit 24;;
+ *) echo "Distribution $DISTRIBUTION is not supported!"; exit 32;;
 esac
 
 apt-get install --no-install-recommends -y libgtk-3-0 libdbus-glib-1-2
 if test $? -ne 0; then
- echo "Install lib error!"; exit 31
+ echo "Install lib error!"; exit 41
 fi
 
-BASE_URL="https://ftp.mozilla.org/pub/firefox/releases/$FIREFOX_VERSION"
+BASE_URL="https://ftp.mozilla.org/pub/firefox/releases/$ISSUER_VERSION"
 
-echo "Download firefox key..."
+echo "Download $ISSUER key..."
 FILE="firefox-pub.gpg"
 rm /tmp/$FILE
-curl -f "$BASE_URL/KEY" -o /tmp/$FILE && gpg --import /tmp/$FILE || exit 41
+curl -f "$BASE_URL/KEY" -o /tmp/$FILE && gpg --import /tmp/$FILE || exit 51
 rm /tmp/$FILE
 
-echo "Download firefox ${FIREFOX_VERSION}..."
-FILE="firefox-${FIREFOX_VERSION}.tar.bz2"
+echo "Download $ISSUER ${ISSUER_VERSION}..."
+FILE="firefox-${ISSUER_VERSION}.tar.bz2"
 rm /tmp/$FILE
 curl -f "$BASE_URL/$DISTRIBUTION/$LANGUAGE/$FILE" -o /tmp/$FILE
 if test $? -ne 0; then
- echo "Download firefox error!"; exit 42
+ echo "Download $ISSUER error!"; exit 52
 fi
 
-echo "Download firefox $FIREFOX_VERSION signature..."
+echo "Download $ISSUER $ISSUER_VERSION signature..."
 rm /tmp/${FILE}.asc
 curl -f "$BASE_URL/$DISTRIBUTION/$LANGUAGE/${FILE}.asc" -o /tmp/${FILE}.asc
 if test $? -ne 0; then
- echo "Download firefox signature error!"; exit 43
+ echo "Download $ISSUER signature error!"; exit 53
 fi
 
-gpg --verify /tmp/${FILE}.asc /tmp/$FILE || exit 44
+gpg --verify /tmp/${FILE}.asc /tmp/$FILE || exit 61
 rm /tmp/${FILE}.asc
 
-echo "Unzip firefox ${FIREFOX_VERSION}..."
+echo "Unzip $ISSUER ${ISSUER_VERSION}..."
 rm -rf /tmp/firefox
 tar -xf /tmp/$FILE -C /tmp
 if test $? -ne 0; then
- echo "Unzip firefox error!"; exit 51
+ echo "Unzip $ISSUER error!"; exit 71
 fi
 
-echo "Install firefox ${FIREFOX_VERSION}..."
+echo "Install $ISSUER ${ISSUER_VERSION}..."
 if [ ! -d "/opt/mozilla" ]; then
- mkdir "/opt/mozilla" || exit 52 # todo
+ mkdir "/opt/mozilla" || exit 72 # todo
 fi
-mv /tmp/firefox "/opt/mozilla/firefox-$FIREFOX_VERSION"
+mv /tmp/firefox "/opt/mozilla/firefox-$ISSUER_VERSION"
 if test $? -ne 0; then
- echo "Install firefox error!"; exit 53
+ echo "Install $ISSUER error!"; exit 81
 fi
-
 rm /tmp/$FILE
-# todo sudo error
-# echo "Running firefox ${FIREFOX_VERSION}..."
-# /opt/mozilla/firefox-${FIREFOX_VERSION}/firefox --version
-# if test $? -ne 0; then
-#  echo "Running firefox error!"; exit 54
-# fi
-
-exit 0
