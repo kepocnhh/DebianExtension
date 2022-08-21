415e0707bddb38e631ceded2f3de8c3eacef1286
Stanley Wintergreen <kepocnhh@gmail.com>
2022-08-21 23:03:57 +0300

    fix install python

--- core/util/python/install.sh
+++ core/util/python/install.sh
@@ -1,52 +1,53 @@
 #!/bin/bash
 
-if [ ! -d "$DEBIAN_EXTENSION_HOME" ]; then
- echo "Dir $DEBIAN_EXTENSION_HOME does not exist!"; exit 11
-fi
+for it in DEBIAN_EXTENSION_HOME; do
+ if [ ! -d "${!it}" ]; then echo "Dir $it does not exist!"; exit 11; fi; done
 
-LATEST_VERSIONS="$(curl -s https://www.python.org/ftp/python/ \
+ISSUER=python
+
+LATEST_VERSIONS="$(curl -s --max-time 2 https://www.python.org/ftp/python/ \
  | grep -E '^<a href="[1-9].[0-9]' \
  | grep -Po '(?<=<a href=")[1-9].[0-9]+(.[0-9]+)?' \
  | sort -V | tail -n 16)"
 if test $? -ne 0; then
- echo "Get latest versions python error!"; exit 12
+ echo "Get latest versions $ISSUER error!"; exit 12
 elif test -z "$LATEST_VERSIONS"; then
- echo "Latest versions python is empty!"; exit 13
+ echo "Latest versions $ISSUER is empty!"; exit 13
 fi
 
-PYTHON_VERSION=""
+ISSUER_VERSION=""
 echo "
 Latest 16 versions:
 $LATEST_VERSIONS
 
-Enter python version:"
+Enter $ISSUER version:"
 while : ; do
  read -n1 char
  if test -z $char; then
   echo; break
  else
-  PYTHON_VERSION+=$char
+  ISSUER_VERSION+=$char
  fi
 done
 
-for it in PYTHON_VERSION; do
+for it in ISSUER_VERSION; do
  if test -z "${!it}"; then echo "$it is empty!"; exit 21; fi; done
 
-if test -d "/opt/Python-$PYTHON_VERSION"; then
- echo "Python ${PYTHON_VERSION} exists!"; exit 22
+if test -d "/opt/Python-$ISSUER_VERSION"; then
+ echo "${ISSUER^} $ISSUER_VERSION exists!"; exit 22
 fi
 
-docker stop python$PYTHON_VERSION
-docker rm python$PYTHON_VERSION
+docker stop python$ISSUER_VERSION
+docker rm python$ISSUER_VERSION
 docker build \
- --build-arg PYTHON_VERSION=$PYTHON_VERSION \
+ --build-arg PYTHON_VERSION=$ISSUER_VERSION \
  -f=$DEBIAN_EXTENSION_HOME/core/util/python/Dockerfile \
- -t=python:$PYTHON_VERSION $DEBIAN_EXTENSION_HOME/core/util/python/ \
- && docker run --name=python$PYTHON_VERSION python:$PYTHON_VERSION \
- && docker cp python$PYTHON_VERSION:/opt/Python-${PYTHON_VERSION} /opt/ \
- && docker stop python$PYTHON_VERSION && docker rm python$PYTHON_VERSION
+ -t=python:$ISSUER_VERSION $DEBIAN_EXTENSION_HOME/core/util/python/ \
+ && docker run --name=python$ISSUER_VERSION python:$ISSUER_VERSION \
+ && docker cp python$ISSUER_VERSION:/opt/Python-$ISSUER_VERSION /opt/ \
+ && docker stop python$ISSUER_VERSION && docker rm python$ISSUER_VERSION
 
-/opt/Python-${PYTHON_VERSION}/python --version
+/opt/Python-${ISSUER_VERSION}/python --version
 if test $? -ne 0; then
- echo "Running python $PYTHON_VERSION error!"; exit 31
+ echo "Running $ISSUER $ISSUER_VERSION error!"; exit 31
 fi
