6e2f998cf72484233ada23b3339ec577bc71ed1c
Stanley Wintergreen <kepocnhh@gmail.com>
2022-08-21 14:21:59 +0300

    fix install sublime

--- xserver/desktop/install_sublime.sh
+++ xserver/desktop/install_sublime.sh
@@ -1,74 +1,98 @@
 #!/bin/bash
 
-if test $# -ne 1; then
-  echo "Script needs for 1 arguments but actual $#!"; exit 11
+for it in DEBIAN_EXTENSION_HOME HOME; do
+ if [ ! -d "${!it}" ]; then echo "Dir $it does not exist!"; exit 11; fi; done
+
+ISSUER=sublime
+
+LATEST_VERSIONS="$(curl -s --max-time 2 https://www.sublimetext.com/download \
+ | grep '>Build [0-9]' \
+ | grep -Po '(?<=<h3>Build )[1-9][0-9]+(?=</h3>)' \
+ | sort -V)"
+if test $? -ne 0; then
+ echo "Get latest versions $ISSUER error!"; exit 12
+elif test -z "$LATEST_VERSIONS"; then
+ echo "Latest versions $ISSUER is empty!"; exit 13
 fi
 
-for it in DEBIAN_EXTENSION_HOME HOME; do
- if [ ! -d "${!it}" ]; then echo "Dir $it does not exist!"; exit 12; fi; done
+ISSUER_VERSION=""
+echo "
+Latest versions:
+$LATEST_VERSIONS
 
-SUBLIME_VERSION=$1
-DISTRIBUTION='x64'
+Enter $ISSUER version:"
+while : ; do
+ read -n1 char
+ if test -z $char; then
+  echo; break
+ else
+  ISSUER_VERSION+=$char
+ fi
+done
 
-for it in SUBLIME_VERSION DISTRIBUTION; do
+ISSUER_VERSION=$1
+MACHINE_HARDWARE_NAME="$(/usr/bin/uname -m)"
+
+for it in ISSUER_VERSION MACHINE_HARDWARE_NAME; do
  if test -z "${!it}"; then echo "$it is empty!"; exit 21; fi; done
 
-if test -d "/opt/sublime-$SUBLIME_VERSION"; then
- echo "Sublime $SUBLIME_VERSION exists!"; exit 22
+if test -d "/opt/sublime-$ISSUER_VERSION"; then
+ echo "${ISSUER^} $ISSUER_VERSION exists!"; exit 22
 fi
 
-case "$DISTRIBUTION" in
- 'x64') /bin/true;; # ignored
- *) echo "Distribution $DISTRIBUTION is not supported!"; exit 23;;
+case "$MACHINE_HARDWARE_NAME" in
+ 'x86_64') DISTRIBUTION='x64';;
+ *) echo "Distribution $MACHINE_HARDWARE_NAME is not supported!"; exit 23;;
 esac
 
 BASE_URL="https://download.sublimetext.com"
 
-echo "Download sublime key..."
+echo "Download $ISSUER key..."
 FILE="sublimehq-pub.gpg"
 rm /tmp/$FILE
-curl -f "$BASE_URL/$FILE" -o /tmp/$FILE && gpg --import /tmp/${FILE} || exit 31
+curl -f "$BASE_URL/$FILE" -o /tmp/$FILE && gpg --import /tmp/$FILE || exit 31
 rm /tmp/$FILE
 
-echo "Download sublime ${SUBLIME_VERSION}..."
-FILE="sublime_text_build_${SUBLIME_VERSION}_${DISTRIBUTION}.tar.xz"
+echo "Download $ISSUER ${ISSUER_VERSION}..."
+FILE="sublime_text_build_${ISSUER_VERSION}_${DISTRIBUTION}.tar.xz"
 rm /tmp/$FILE
 curl -f "$BASE_URL/$FILE" -o /tmp/$FILE
 if test $? -ne 0; then
- echo "Download sublime error!"; exit 32
+ echo "Download $ISSUER error!"; exit 32
 fi
 
-echo "Download sublime $SUBLIME_VERSION signature..."
+echo "Download $ISSUER $ISSUER_VERSION signature..."
 rm /tmp/${FILE}.asc
 curl -f "$BASE_URL/${FILE}.asc" -o /tmp/${FILE}.asc
 if test $? -ne 0; then
- echo "Download sublime signature error!"; exit 33
+ echo "Download $ISSUER $ISSUER_VERSION signature error!"; exit 32
 fi
 
-gpg --verify /tmp/${FILE}.asc /tmp/$FILE || exit 34
+gpg --verify /tmp/${FILE}.asc /tmp/$FILE || exit 33
 rm /tmp/${FILE}.asc
 
-echo "Unzip sublime ${SUBLIME_VERSION}..."
+echo "Unzip $ISSUER ${ISSUER_VERSION}..."
 rm -rf /tmp/sublime_text
 tar -xf /tmp/$FILE -C /tmp
 if test $? -ne 0; then
- echo "Unzip sublime $SUBLIME_VERSION error!"; exit 41
+ echo "Unzip $ISSUER $ISSUER_VERSION error!"; exit 41
 fi
 rm /tmp/$FILE
 
-echo "Install sublime ${SUBLIME_VERSION}..."
-mv /tmp/sublime_text "/opt/sublime-$SUBLIME_VERSION"
+echo "Install $ISSUER ${ISSUER_VERSION}..."
+mv /tmp/sublime_text "/opt/sublime-$ISSUER_VERSION"
 if test $? -ne 0; then
- echo "Install sublime $SUBLIME_VERSION error!"; exit 42
+ echo "Install $ISSUER $ISSUER_VERSION error!"; exit 42
 fi
 
 RESULT_PATH=$HOME/.config/sublime-text/Packages/User/
 mkdir -p $RESULT_PATH
 cp $DEBIAN_EXTENSION_HOME/xserver/desktop/config/sublime/* $RESULT_PATH
 if test $? -ne 0; then
- echo "Install sublime config error!"; exit 42
+ echo "Install $ISSUER config error!"; exit 43
 fi
 
-/opt/sublime-${SUBLIME_VERSION}/sublime_text --version || exit 44
-
-exit 0
+/opt/sublime-${ISSUER_VERSION}/sublime_text --version
+if test $? -ne 0; then
+ echo "Running $ISSUER $ISSUER_VERSION error!"; exit 44
+fi
