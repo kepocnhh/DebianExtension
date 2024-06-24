957865335b80f4fc48834a1d2d9d7a8870be2bd5
Stanley Wintergreen <kepocnhh@gmail.com>
2024-06-24 23:18:27 +0300

    wpa supplicant

--- core/network/wireless/wpa_supplicant_conf_create.sh
+++ core/network/wireless/wpa_supplicant_conf_create.sh
@@ -1,48 +1,31 @@
-echo "create wpa_supplicant.conf..."
+#!/bin/bash
 
-ERROR_CODE_SERVICE=1
-ERROR_CODE_EMPTY_SSID=2
-ERROR_CODE_EMPTY_PASSWORD=3
-ERROR_CODE_WPA_PASSPHRASE=4
-ERROR_CODE_MOVE_CONFIG=5
+echo 'Create wpa_supplicant.conf...'
 
-ARGUMENT_COUNT_EXPECTED=2
-if test $# -ne $ARGUMENT_COUNT_EXPECTED; then
-    echo "Script needs for $ARGUMENT_COUNT_EXPECTED arguments but actual $#"
-    exit $ERROR_CODE_SERVICE
-fi
+if test $# -ne 1; then
+ echo "Script needs for 2 arguments but actual $#!"; exit 12; fi
 
-SSID=$1
-PASSWORD=$2
+SSID="$1"
+PASSWORD="$2"
 
-if test -z $SSID; then
-    echo "SSID must be not empty!"
-    exit $ERROR_CODE_EMPTY_SSID
-fi
-if test -z $PASSWORD; then
-    echo "PASSWORD must be not empty!"
-    exit $ERROR_CODE_EMPTY_PASSWORD
-fi
+for it in SSID PASSWORD; do
+ if test -z "${!it}"; then echo "$it is empty!"; exit 13; fi; done
 
-TMP_FILE_PATH=/tmp/wpa_supplicant.tmp.conf
+TMP_FILE_PATH="/tmp/wpa_supplicant.tmp.conf"
 
 STATUS=0
 
-/usr/bin/wpa_passphrase "$SSID" "$PASSWORD" >> "$TMP_FILE_PATH" || STATUS=$?
-if test $STATUS -ne 0; then
-	echo "create wpa_supplicant.conf error!"
-	rm "$TMP_FILE_PATH"
-    exit $ERROR_CODE_WPA_PASSPHRASE
-fi
+/usr/bin/wpa_passphrase "$SSID" "$PASSWORD" >> "$TMP_FILE_PATH"
+
+if test $? -ne 0; then
+ echo 'Create wpa_supplicant.conf error!'; rm "$TMP_FILE_PATH"; exit 14; fi
 
 RESULT_FILE_PATH="/etc/wpa_supplicant.$SSID.conf"
-mv "$TMP_FILE_PATH" "$RESULT_FILE_PATH" || STATUS=$?
-if test $STATUS -ne 0; then
-	echo "move wpa_supplicant.conf error!"
-	rm "$TMP_FILE_PATH"
-    exit $ERROR_CODE_MOVE_CONFIG
-fi
+mv "$TMP_FILE_PATH" "$RESULT_FILE_PATH"
 
-echo "create \"$RESULT_FILE_PATH\" success."
+if test $? -ne 0; then
+ echo 'Move wpa_supplicant.conf error!'; rm "$TMP_FILE_PATH"; exit 15; fi
+
+echo "Create \"$RESULT_FILE_PATH\" success."
 
 exit 0
