c9ea9949d4cde0bf34ade11c7c2b5cce7fe855ec
Stanley Wintergreen <kepocnhh@gmail.com>
2024-06-24 23:22:17 +0300

    wpa supplicant

--- core/network/wireless/wpa_supplicant_conf_create.sh
+++ core/network/wireless/wpa_supplicant_conf_create.sh
@@ -11,20 +11,14 @@ PASSWORD="$2"
 for it in SSID PASSWORD; do
  if test -z "${!it}"; then echo "$it is empty!"; exit 13; fi; done
 
-TMP_FILE_PATH="/tmp/wpa_supplicant.tmp.conf"
-
-STATUS=0
-
-/usr/bin/wpa_passphrase "$SSID" "$PASSWORD" >> "$TMP_FILE_PATH"
-
-if test $? -ne 0; then
- echo 'Create wpa_supplicant.conf error!'; rm "$TMP_FILE_PATH"; exit 14; fi
-
 RESULT_FILE_PATH="/etc/wpa_supplicant.$SSID.conf"
-mv "$TMP_FILE_PATH" "$RESULT_FILE_PATH"
+if test -f "$RESULT_FILE_PATH"; then
+ echo "Config \"$RESULT_FILE_PATH\" already exists!"; exit 21; fi
+
+/usr/bin/wpa_passphrase "$SSID" "$PASSWORD" >> "$RESULT_FILE_PATH"
 
 if test $? -ne 0; then
- echo 'Move wpa_supplicant.conf error!'; rm "$TMP_FILE_PATH"; exit 15; fi
+ echo 'Create wpa_supplicant.conf error!'; rm "$RESULT_FILE_PATH"; exit 14; fi
 
 echo "Create \"$RESULT_FILE_PATH\" success."
 
